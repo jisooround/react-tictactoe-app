4048f96fe7f6240572000a101726165823e1dbb8
jisooround <9utesleben@gmail.com>
2022-12-14 20:41:16 +0900

    first commit

---
+++ src/Test.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react'
+
+const Test = () => {
+  const [value, setValue] = useState('');
+  const [listArray, setListArray] = useState(['haha', 'hoho'])
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setListArray(prevList => {
+      return [value,...prevList];
+    })
+    setValue('');
+  }
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input value={value} onChange={e => setValue(e.target.value)} />
+        <button type="submit">Add</button>
+      </form>
+      <ul>
+        {listArray.map(item => (
+          <li>{item}</li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default Test
